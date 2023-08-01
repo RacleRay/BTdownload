4005608eef2c71d2679f62f6d1663e5159ed431e
RacleRay <racleme@outlook.com>
2023-08-01 11:22:32 +0800

    bitmap

--- src/main.c
+++ src/main.c
@@ -8,6 +8,23 @@
 #include "../include/log.h"
 
 
+void test_bitmap() {
+    Bitmap map1, map2;
+    unsigned char bf1[2] = { 0xa0, 0xa0 };
+    unsigned char bf2[2] = { 0xc0, 0xc0 };
+
+    map1.bitfield        = bf1;
+    map1.bitfield_length = 2;
+    map1.valid_length    = 11;
+    map2.bitfield        = bf2;
+    map2.bitfield_length = 2;
+    map2.valid_length    = 11;
+
+    int ret = is_interested(&map1,&map2);
+    printf("%d\n",ret);
+}
+
+
 int main(int argc, char *argv[])
 {
 	int ret;
@@ -20,6 +37,10 @@ int main(int argc, char *argv[])
 	// 解析种子文件
 	ret = parse_metafile(argv[1]);
 	if(ret != 0)  { printf("%s:%d error\n",__FILE__,__LINE__); return -1; }
+
+
+    test_bitmap();
+
 
 	return 0;
 }
